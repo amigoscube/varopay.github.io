b8cad4b48c54dec3f51ec72ae5ffd85542c0221e
divyatejakandhadi <divya.kandhadi@amigoscube.com>
2014-09-04 12:11:21 +0530

    SourcePushCommit

---
+++ Varopay/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Varopay.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Varopay/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Varopay.Global" Language="C#" %>
