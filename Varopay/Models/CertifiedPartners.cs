f50d0da2b1cd6978d28a92ac11a84a992bd63bbb
divyatejakandhadi <divya.kandhadi@amigoscube.com>
2014-10-18 17:31:30 +0530

    18/10/2014-05:31

---
+++ Varopay/Models/CertifiedPartners.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Varopay.Models
+{
+    public class CertifiedPartner
+    {
+        public Guid CertifiedPartnerID{ get; set; }
+        public String PartnerName { get; set; }
+
+        [DataType(DataType.Url)]
+        public String WebsiteUrl { get; set; }
+
+        [DataType(DataType.ImageUrl)]
+        public String ImageUrl{ get; set; }
+        public String Description { get; set; }
+    }
+}
